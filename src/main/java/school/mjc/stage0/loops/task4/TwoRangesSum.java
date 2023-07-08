7663de6e5ee5b1710065a64a232f24f8db51c975
NeverRIEght <lfkomarovme@gmail.com>
2023-07-08 14:52:43 +0300

    Sum of skipped

--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -2,6 +2,28 @@ package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+        boolean testsStatus = true;
+        if(numberToSkip > lastInRow) {
+            System.out.println("number to skip is bigger then the last");
+            testsStatus = false;
+        }
+        if(lastInRow < 0) {
+            System.out.println("last number in row is negative");
+            testsStatus = false;
+        }
 
+        if(testsStatus) {
+            int skippedSum = 0;
+            int countedSum = 0;
+            for(int i = 1; i <= lastInRow; i++) {
+                if(i % numberToSkip == 0) {
+                    skippedSum += i;
+                } else {
+                    countedSum += i;
+                }
+            }
+            System.out.println("skipped sum is " + skippedSum);
+            System.out.println("counted sum is " + countedSum);
+        }
     }
 }
